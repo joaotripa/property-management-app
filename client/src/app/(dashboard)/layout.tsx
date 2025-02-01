e285401417e2d6ca9c425e29ada235c9b7e685d2
João Tripa <jfptripa@gmail.com>
2025-02-01 17:31:56 +0000

    Resctructure project folders, create dashboard navbar and dashboard sidebar

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,16 +1,22 @@
 "use client";
+import { DashboardSidebar } from "@/components/DashboardSidebar";
+import { CustomSidebar } from "@/components/CustomSidebar";
 import Navbar from "@/components/Navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 
 export default function DashboardLayout({ children }: {children: React.ReactNode;}) {
   return (
-    <div className="dashboard">
-      <div className="dashboard__content">
-        <Navbar/>
-        <main className="dashboard__body">
-            {children}
-        </main>
+    <SidebarProvider>
+      <div className="dashboard">
+        <DashboardSidebar/>
+        <div className="dashboard__content">
+          <Navbar/>
+          <main className="dashboard__body">
+              {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   )
 }
