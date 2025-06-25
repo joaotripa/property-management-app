bb8e5658398742ef89e56772b453a4cdaf430ca0
joaotripa <joaotripa96@gmail.com>
2025-06-25 17:18:21 +0800

    Add integration to Umami to collect data analytics.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
 import { Montserrat, Nunito_Sans } from "next/font/google";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -20,6 +21,13 @@ const nunitoSans = Nunito_Sans({
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${montserrat.variable} ${nunitoSans.variable}`}>
+      <head>
+        <Script
+          defer
+          src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        ></Script>
+      </head>
       <body className="font-body" suppressHydrationWarning>
         <Toaster richColors closeButton theme="light" />
         {children}
